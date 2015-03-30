b2b6a38eef31929f318b111487bb50af1e8993e7
madidas <matias.turunen@gmail.com>
2015-03-30 14:09:26 +0300

    Added room....

---
+++ game/src/game/Room.java
@@ -0,0 +1,18 @@
+package game;
+
+public class Room {
+	
+	//HEhehhehfuewhiuhfhewaof
+
+	class TrapRoom extends Room {
+		
+	}
+	
+	class PuzzleRoom extends Room {
+		
+	}
+	
+	class ObstacleRoom extends Room {
+		
+	}
+}
