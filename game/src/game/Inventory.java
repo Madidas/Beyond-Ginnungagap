b187e9ec1344d05b79a1b89fe5ebf6f2f4b9753a
joonaoko <joona.koskivaara@gmail.com>
2015-03-31 10:17:46 +0300

    asd

--- game/src/game/Inventory.java
+++ game/src/game/Inventory.java
@@ -1,5 +1,16 @@
 package game;
 
-public class Inventory {
+import java.util.ArrayList;
 
+public class Inventory {
+	private static Item inventoryItem;
+	
+	public Inventory(Item inventoryItem){
+		Inventory.inventoryItem = inventoryItem;
+	}
+
+	public ArrayList<Item> getInventory(){
+		ArrayList<Item> itemInventory = new ArrayList<Item>();
+		return itemInventory;
+	}
 }
---
+++ game/src/game/Item.java
@@ -0,0 +1,8 @@
+package game;
+
+public class Item {
+	private String 	itemType 	= "Type";
+	private String 	itemName 	= "Name";
+	private String 	itemAction 	= "Action";
+	private int 	itemWeight 	= 1;
+}
