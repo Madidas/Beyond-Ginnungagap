e8c859b0863fc649d5646e634122d19669194ac7
joonaoko <joona.koskivaara@gmail.com>
2015-03-31 13:12:36 +0300

    Added inputreader

---
+++ game/src/game/InputReader.java
@@ -0,0 +1,14 @@
+package game;
+
+import java.util.*;
+
+public class InputReader {
+	
+	Scanner playerInput = new Scanner(System.in);
+	
+	public String getPlayerInput() {
+		System.out.println("Type something:");
+		String inputLine = playerInput.nextLine();
+		return inputLine;
+	}
+}
