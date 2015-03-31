c65eba327d98d42f443a717f146bb843fb84da5e
joonaoko <joona.koskivaara@gmail.com>
2015-03-31 11:27:20 +0300

    asd

--- game/src/game/Item.java
+++ game/src/game/Item.java
@@ -4,8 +4,18 @@ import java.util.ArrayList;
 
 public class Item {
 	private String itemName = "Name";
+	private String itemDescription = "Description";
 	
 	ArrayList<String> itemProperties = new ArrayList<String>();
+	
+	public interface IsUsable {
+	}
+	
+	public interface IsReadable {
+	}
+	
+	public interface IsThrowable {
+	}
 	
 	public interface IsEdible {
 	}
