4b0fa2b7690763eb8ed33ed84cb3a25839b0e8d7
joonaoko <joona.koskivaara@gmail.com>
2015-04-13 14:02:18 +0300

    dasdvgseawedfcv

--- game/src/game/Player.java
+++ game/src/game/Player.java
@@ -1,10 +1,16 @@
 package game;
 
+import game.Room.TestRoom;
+
 public class Player  {
 	private String playerName = "Viking";
 
 	private class PlayerInventory {
+		private static Inventory playerInventory;
 		
+		public PlayerInventory(Inventory playerInventory) {
+			PlayerInventory.playerInventory = playerInventory;
+		}
 	}
 	
 	private class PlayerLocation {
