d7dde031e41213857942df1e24a38cd90fb0112e
madidas <matias.turunen@gmail.com>
2015-04-14 12:40:10 +0300

    ???????????

--- game/src/game/GameEngine.java
+++ game/src/game/GameEngine.java
@@ -2,10 +2,13 @@ package game;
 
 public class GameEngine {
 
-	InputReader theInputReader = new InputReader();
+	InputReader theInputReader = new InputReader(); //Here game controller
+	
+	Player thePlayer = new Player();
+	Room theTestRoom = new TestRoom();
 	
 	while(true) {
-		//Game logic
+		
 	}
 }
 
