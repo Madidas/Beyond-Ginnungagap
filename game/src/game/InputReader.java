d1ed029490ef5e2758111feca561f2f16cd717e3
madidas <matias.turunen@gmail.com>
2015-04-30 12:27:00 +0300

    InputReader modifying

--- game/src/game/InputReader.java
+++ game/src/game/InputReader.java
@@ -7,15 +7,18 @@ public class InputReader {
 	static Scanner playerInput = new Scanner(System.in);
 
 	public static String getPlayerInput() {
-		System.out.println("Type something:");
-		String inputLine = playerInput.nextLine();
-		//System.out.println(inputLine);
-		inputLine = inputLine.toUpperCase();
-		if (inputLine.length !== 0) {
-			return inputLine;
-			System.out.println();
-		} else {
-			System.out.println("Try again you failure.");
-		}
+		do {
+			System.out.println("Type something:");
+			String inputLine = playerInput.nextLine();
+			//System.out.println(inputLine);
+			inputLine = inputLine.toUpperCase();
+			if (inputLine.length() !== 0) {
+				x = 1;
+				System.out.println();
+				return inputLine;
+			} else {
+				System.out.println("Try again you failure.");
+			}
+		} while (x == 0);
 	}
 }
