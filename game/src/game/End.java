74a82563f565edd38fe706f260756eb4c06d6d2b
Christwi <christofer.wiik@gmail.com>
2015-05-05 13:13:30 +0300

    2nd ending.

--- game/src/game/End.java
+++ game/src/game/End.java
@@ -2,63 +2,123 @@ package game;
 
 public class End {
 	
-	public static void ending() throws Exception{
+	public static void endingS() throws Exception {
 		
 	
-	int x = 40; //text speed in milliseconds
+		int x = 40; //text speed in milliseconds
 	
-	System.out.println(" ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~");
-	String text1 = "            You see the escape shuttle in front of you and quickly enter it."; 
-	for (int i = 0; i < text1.length(); i++) {
-	    System.out.print(text1.charAt(i));
-	    Thread.sleep(x);
-	}
-	System.out.println();
-	System.out.println();
-	Thread.sleep(500); //small pause
+		System.out.println(" ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~");
+		String text1 = "            You see the escape shuttle in front of you and quickly enter it."; 
+		for (int i = 0; i < text1.length(); i++) {
+			System.out.print(text1.charAt(i));
+			Thread.sleep(x);
+		}
+		System.out.println();
+		System.out.println();
+		Thread.sleep(500); //small pause
 	
-	String text2 = "         You start randomly hitting different buttons on the control panel and "; 
-	for (int i = 0; i < text2.length(); i++) {
-	    System.out.print(text2.charAt(i));
-	    Thread.sleep(x);
-	}
-	System.out.println();
+		String text2 = "         You start randomly hitting different buttons on the control panel and "; 
+		for (int i = 0; i < text2.length(); i++) {
+			System.out.print(text2.charAt(i));
+			Thread.sleep(x);
+		}
+		System.out.println();
+		
+		String text3 = "           before long a text appears on the screen: “Turn on auto-pilot”."; 
+		for (int i = 0; i < text3.length(); i++) {
+			System.out.print(text3.charAt(i));
+			Thread.sleep(x);
+		}
+		System.out.println();
+		System.out.println();
+		Thread.sleep(500); //small pause
 	
-	String text3 = "           before long a text appears on the screen: “Turn on auto-pilot”."; 
-	for (int i = 0; i < text3.length(); i++) {
-	    System.out.print(text3.charAt(i));
-	    Thread.sleep(x);
-	}
-	System.out.println();
-	System.out.println();
-	Thread.sleep(500); //small pause
+		String text4 = "    You press a green button below the screen and suddenly the shuttle starts moving."; 
+		for (int i = 0; i < text4.length(); i++) {
+			System.out.print(text4.charAt(i));
+			Thread.sleep(x);
+		}
+		System.out.println();
+		System.out.println();
+		Thread.sleep(500); //small pause
 	
-	String text4 = "    You press a green button below the screen and suddenly the shuttle starts moving."; 
-	for (int i = 0; i < text4.length(); i++) {
-	    System.out.print(text4.charAt(i));
-	    Thread.sleep(x);
-	}
-	System.out.println();
-	System.out.println();
-	Thread.sleep(500); //small pause
+		String text5 = "                 As you are taking off Death appears before you again:"; 
+		for (int i = 0; i < text5.length(); i++) {
+			System.out.print(text5.charAt(i));
+			Thread.sleep(x);
+		}
+		System.out.println();
+		System.out.println();
+		Thread.sleep(500); //small pause
 	
-	String text5 = "                 As you are taking off Death appears before you again:"; 
-	for (int i = 0; i < text5.length(); i++) {
-	    System.out.print(text5.charAt(i));
-	    Thread.sleep(x);
-	}
-	System.out.println();
-	System.out.println();
-	Thread.sleep(500); //small pause
+		String text6 = "     “You may have gotten off the spaceship, but that doesn’t mean that you’re safe.”"; 
+		for (int i = 0; i < text6.length(); i++) {
+			System.out.print(text6.charAt(i));
+			Thread.sleep(x);
+		}
+		System.out.println();
+		System.out.println();
+		System.out.println(" ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~");
+
+		}
 	
-	String text6 = "     “You may have gotten off the spaceship, but that doesn’t mean that you’re safe.”"; 
-	for (int i = 0; i < text6.length(); i++) {
-	    System.out.print(text6.charAt(i));
-	    Thread.sleep(x);
-	}
-	System.out.println();
-	System.out.println();
-	System.out.println(" ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~");
+	public static void endingCR() throws Exception {
+		
+		
+		int x = 40; //text speed in milliseconds
+	
+		System.out.println(" ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~");
+		String text1 = "      You enter the control room you see an control panel with dozens of flashng lights on it."; 
+		for (int i = 0; i < text1.length(); i++) {
+			System.out.print(text1.charAt(i));
+			Thread.sleep(x);
+		}
+		System.out.println();
+		System.out.println();
+		Thread.sleep(500); //small pause
+	
+		String text2 = "         You start randomly hitting different buttons on the control panel and before long "; 
+		for (int i = 0; i < text2.length(); i++) {
+			System.out.print(text2.charAt(i));
+			Thread.sleep(x);
+		}
+		System.out.println();
+	
+		String text3 = "       a text appears on the screen: “Stabilazing the ship. Embrace for an emergency landing.”"; 
+		for (int i = 0; i < text3.length(); i++) {
+			System.out.print(text3.charAt(i));
+	    	Thread.sleep(x);
+		}
+		System.out.println();
+		System.out.println();
+		Thread.sleep(500); //small pause
+	
+		String text4 = ""; 
+		for (int i = 0; i < text4.length(); i++) {
+			System.out.print(text4.charAt(i));
+			Thread.sleep(x);
+		}
+		System.out.println();
+		System.out.println();
+		Thread.sleep(500); //small pause
+	
+		String text5 = "          As you are preparing yourself for the impact Death appears before you again:"; 
+		for (int i = 0; i < text5.length(); i++) {
+			System.out.print(text5.charAt(i));
+			Thread.sleep(x);
+		}
+		System.out.println();
+		System.out.println();
+		Thread.sleep(500); //small pause
+	
+		String text6 = "         “You may have avoided death this time, but that doesn’t mean that you’re safe.”"; 
+		for (int i = 0; i < text6.length(); i++) {
+			System.out.print(text6.charAt(i));
+			Thread.sleep(x);
+		}
+		System.out.println();
+		System.out.println();
+		System.out.println(" ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~");
 
 	}
 }
