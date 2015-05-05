1c2ab88121fe001d7e05371bb4cd85592fb8436e
joonaoko <joona.koskivaara@gmail.com>
2015-05-05 10:31:01 +0300

    Added second floor to space ship map

--- game/src/game/WorldCreation.java
+++ game/src/game/WorldCreation.java
@@ -20,7 +20,7 @@ public final class WorldCreation
 		Room corridor3		= new Room (3, "Corridor", "You're in a dimly lit corridor. To west there's a vent large enough for you to crawl in. The corridor continues.");
 		Room vent1			= new Room (4, "Vent", "You're crawling in a tight ventilation system.");
 		Room corridor4 		= new Room (3, "Corridor", "You're in a dimly lit corridor. There's an elevator and the corridor continues.");
-		Room elevator1		= new Room (5, "Elevator", "You're inside an elevator. The elevator can go up and down.");
+		Room elevator1		= new Room (5, "Elevator", "You're inside an elevator. The elevator can go up.");
 		Room corridor5 		= new Room (3, "Corridor", "You're in a dimly lit corridor. There's a large storage room on your east side and the corridor continues.");
 		Room corridor6 		= new Room (3, "Corridor", "You're in a dimly lit corridor. There's a large storage room on your east side and the corridor continues.");
 		Room storageroom 	= new Room (6, "Storage Room", "You're in gigantic storage room. There's a vent in the south east corner.");
@@ -31,7 +31,7 @@ public final class WorldCreation
 		Room vent3			= new Room (4, "Vent", "You're crawling in a tight ventilation system.");
 		Room corridor8		= new Room (3, "Corridor", "You're in a dimly lit corridor.");
 		Room corridor10		= new Room (3, "Corridor", "You're in a dimly lit corridor.");
-		Room elevator2		= new Room (5, "Elevator", "You're inside an elevator. The elevator can go up and down.");
+		Room elevator2		= new Room (5, "Elevator", "You're inside an elevator. The elevator can go up.");
 		Room dininghall		= new Room (7, "Dining Hall", "You're in a large dining hall.");
 		
 		Room engineroom		= new Room (8, "Engine Room", "You're in a loud engine room.");
@@ -42,6 +42,28 @@ public final class WorldCreation
 		Room kitchenstorage = new Room (11, "Kitchen Storage Room", "You're in the kitchen's storage room.");
 		Room kitchenfreezer = new Room (12, "Kitchen Freezer", "You're in the kitchen's freezer. It's freezingly cold.");
 		Room vent4			= new Room (4, "Vent", "You're crawling in a tight ventilation system.");
+		
+		Room controlroom	= new Room (13, "Control Room", "You're in the space ship's control room!");
+		Room corridor11		= new Room (3, "Corridor", "You're in a dimly lit corridor.");
+		Room elevator4		= new Room (5, "Elevator", "You're inside an elevator. The elevator can go down.");		
+		Room corridor12		= new Room (3, "Corridor", "You're in a dimly lit corridor.");
+		Room cabin2			= new Room (2, "Cabin", "You're in the captain's cabin.");
+		Room corridor13		= new Room (3, "Corridor", "You're in a dimly lit corridor.");
+		Room vent7			= new Room (4, "Vent", "You're crawling in a tight ventilation system."); 
+		Room corridor14		= new Room (3, "Corridor", "You're in a dimly lit corridor.");
+		
+		Room hangar			= new Room (14, "Hangar", "You've reached the space ship's hangar. There should be an escape shuttle!");
+		Room escapeshuttle	= new Room (15, "Escape Shuttle", "You reached the escape shuttle!");
+		
+		Room corridor15		= new Room (3, "Corridor", "You're in a dimly lit corridor.");
+		Room vent5			= new Room (4, "Vent", "You're crawling in a tight ventilation system.");
+		Room elevator3		= new Room (5, "Elevator", "You're inside an elevator. The elevator can go down.");
+		Room corridor16		= new Room (3, "Corridor", "You're in a dimly lit corridor.");
+		Room vent6			= new Room (4, "Vent", "You're crawling in a tight ventilation system.");
+		
+		Room corridor17		= new Room (3, "Corridor", "You're in a dimly lit corridor.");
+		Room vent8			= new Room (4, "Vent", "You're crawling in a tight ventilation system.");
+		
 
 		// Create exit objects
 		Exit bunkout 				= new Exit ( Exit.OUT, cabin );
@@ -59,14 +81,14 @@ public final class WorldCreation
 		
 		Exit vent1east				= new Exit (Exit.EAST, vent2);
 		Exit vent1north				= new Exit (Exit.NORTH,vent3);
-		//Exit vent1up				= new Exit (Exit.UP, vent5);
+		Exit vent1up				= new Exit (Exit.UP, vent5);
 		Exit vent1out				= new Exit (Exit.OUT, corridor3);
 		
 		Exit corridor4in			= new Exit (Exit.IN, elevator1);
 		Exit corridor4north			= new Exit (Exit.NORTH, corridor5);
 		Exit corridor4west			= new Exit (Exit.WEST, corridor3);
 		
-		//Exit elevator1up			= new Exit (Exit.UP, elevator3);
+		Exit elevator1up			= new Exit (Exit.UP, elevator3);
 		Exit elevator1out			= new Exit (Exit.UP, corridor4);
 		
 		Exit corridor5north			= new Exit (Exit.NORTH, corridor6);
@@ -83,7 +105,7 @@ public final class WorldCreation
 		
 		Exit vent2west				= new Exit (Exit.WEST, vent1);
 		Exit vent2north				= new Exit (Exit.NORTH,vent4);
-		//Exit vent2up				= new Exit (Exit.UP, vent6);
+		Exit vent2up				= new Exit (Exit.UP, vent6);
 		Exit vent2out				= new Exit (Exit.OUT, storageroom);
 		
 		Exit corridor7west			= new Exit (Exit.WEST, corridor8);
@@ -99,14 +121,14 @@ public final class WorldCreation
 		Exit corridor2in			= new Exit (Exit.IN, vent3);
 		
 		Exit vent3south				= new Exit (Exit.SOUTH, vent1);
-		//Exit vent3up				= new Exit (Exit.UP, vent7);
+		Exit vent3up				= new Exit (Exit.UP, vent7);
 		Exit vent3out				= new Exit (Exit.OUT, corridor2);
 		
 		Exit corridor10in			= new Exit (Exit.IN, elevator2);
 		Exit corridor10east			= new Exit (Exit.EAST, dininghall);
 		Exit corridor10north		= new Exit (Exit.NORTH, engineroom);
 		
-		//Exit elevator2up			= new Exit (Exit.UP, elevator4);
+		Exit elevator2up			= new Exit (Exit.UP, elevator4);
 		Exit elevator2out			= new Exit (Exit.UP, corridor10);
 		
 		Exit dininghalleast			= new Exit (Exit.EAST, corridor10);
@@ -129,11 +151,60 @@ public final class WorldCreation
 		Exit kitchensoutheast		= new Exit (Exit.SOUTHEAST, kitchenfreezer);
 		
 		Exit vent4south				= new Exit (Exit.SOUTH, vent2);
-		//Exit vent4up				= new Exit (Exit.UP, vent8);
+		Exit vent4up				= new Exit (Exit.UP, vent8);
 		Exit vent4out				= new Exit (Exit.OUT, kitchen);
 		
 		Exit kitchenstoragewest		= new Exit (Exit.WEST, kitchen);
 		Exit kitchenfreezerwest		= new Exit (Exit.WEST, kitchen);
+		
+		Exit controlroomsouthwest	= new Exit (Exit.SOUTHWEST, corridor11);
+		Exit controlroomsoutheast	= new Exit (Exit.SOUTHEAST, corridor17);
+		
+		Exit corridor11in			= new Exit (Exit.IN, elevator4);
+		Exit corridor11north		= new Exit (Exit.NORTH, controlroom);
+		Exit corridor11south		= new Exit (Exit.SOUTH, corridor12);
+		
+		Exit elevator4down			= new Exit (Exit.DOWN, elevator2);
+		Exit elevator4out			= new Exit (Exit.OUT, corridor11);
+
+		Exit corridor12in			= new Exit (Exit.IN, cabin2);
+		Exit corridor12north		= new Exit (Exit.NORTH, corridor11);
+		Exit corridor12south		= new Exit (Exit.SOUTH, corridor13);
+		
+		Exit cabin2out				= new Exit (Exit.OUT, corridor12);
+		
+		Exit corridor13in			= new Exit (Exit.IN, vent7);
+		Exit corridor13south		= new Exit (Exit.SOUTH, corridor14);
+		Exit corridor13north		= new Exit (Exit.NORTH, corridor12);
+		
+		Exit vent7down				= new Exit (Exit.DOWN, vent3);
+		Exit vent7south				= new Exit (Exit.SOUTH, vent5);
+		Exit vent7out				= new Exit (Exit.OUT, corridor13);
+		
+		Exit corridor14west			= new Exit (Exit.WEST, hangar);
+		Exit corridor14east			= new Exit (Exit.EAST, corridor15);
+		Exit corridor14in 			= new Exit (Exit.IN, vent5);
+		
+		Exit hangarin				= new Exit (Exit.IN, escapeshuttle);
+		Exit hangareast				= new Exit (Exit.EAST, corridor14);
+		
+		
+		Exit corridor15in			= new Exit (Exit.IN, elevator3);
+		Exit corridor15west			= new Exit (Exit.WEST, corridor14);
+		Exit corridor15north		= new Exit (Exit.NORTH, corridor16);
+		
+		Exit corridor16in			= new Exit (Exit.IN, vent6);
+		Exit corridor16north		= new Exit (Exit.NORTH, corridor17);
+		Exit corridor16south		= new Exit (Exit.SOUTH, corridor15);
+		
+		Exit corridor17in			= new Exit (Exit.IN, vent8);
+		Exit corridor17north		= new Exit (Exit.NORTH, controlroom);
+		Exit corridor17south		= new Exit (Exit.SOUTH, corridor16);
+		
+		Exit vent8down				= new Exit (Exit.DOWN, vent4);
+		Exit vent8south				= new Exit (Exit.SOUTH, vent6);
+		Exit vent8out				= new Exit (Exit.OUT, corridor17);
+		
 
 		// Adding exits to correct rooms
 		bunk.addExit(bunkout);
@@ -156,9 +227,9 @@ public final class WorldCreation
 		corridor6.addExit(corridor6south);
 	    vent1.addExit(vent1east);				
 	    vent1.addExit(vent1north);				
-		//vent1.addExit(vent1up);
+		vent1.addExit(vent1up);
 	    vent1.addExit(vent1out);
-		//elevator1.addExit(elevator1up);			
+		elevator1.addExit(elevator1up);			
 			               		                
 	    storageroom.addExit(storageroomnorth);		
 	    storageroom.addExit(storageroomsouth);		
@@ -166,7 +237,7 @@ public final class WorldCreation
 			                       
 	    vent2.addExit(vent2west);				
 	    vent2.addExit(vent2north);				
-		//vent2.addExit(vent2up);
+		vent2.addExit(vent2up);
 	    vent2.addExit(vent2out);
 	    corridor7.addExit(corridor7west);
 	    corridor7.addExit(corridor7north);	 
@@ -178,12 +249,12 @@ public final class WorldCreation
 	    corridor2.addExit(corridor2east);
 	    corridor2.addExit(corridor2in);
 	    vent3.addExit(vent3south);
-	    //vent3.addExit(vent3up);
+	    vent3.addExit(vent3up);
 	    vent3.addExit(vent1out);
 	    corridor10.addExit(corridor10in);	 
 	    corridor10.addExit(corridor10east);	 
 	    corridor10.addExit(corridor10north);	 
-	    //elevator2.addExit(elevator2up);
+	    elevator2.addExit(elevator2up);
 	    elevator2.addExit(elevator2out);
 	    dininghall.addExit(dininghalleast);	 
 	    dininghall.addExit(dininghallwest);	 
@@ -200,10 +271,58 @@ public final class WorldCreation
 	    kitchen.addExit(kitcheneast);	
 	    kitchen.addExit(kitchensoutheast);	
 	    vent4.addExit(vent4south);
-	    //vent4.addExit(vent4up);
+	    vent4.addExit(vent4up);
 	    vent4.addExit(vent4out);
 	    kitchenstorage.addExit(kitchenstoragewest);
 		kitchenfreezer.addExit(kitchenfreezerwest);	
+		
+		controlroom.addExit(controlroomsouthwest);
+		controlroom.addExit(controlroomsoutheast);
+		
+		corridor11.addExit(corridor11in);
+		corridor11.addExit(corridor11north);
+		corridor11.addExit(corridor11south);
+		
+		elevator4.addExit(elevator4down);
+		elevator4.addExit(elevator4out);
+
+		corridor12.addExit(corridor12in);
+		corridor12.addExit(corridor12north);
+		corridor12.addExit(corridor12south);
+		
+		cabin2.addExit(cabin2out);
+		
+		corridor13.addExit(corridor13in);
+		corridor13.addExit(corridor13south);
+		corridor13.addExit(corridor13north);
+		
+		vent7.addExit(vent7down);
+		vent7.addExit(vent7south);
+		vent7.addExit(vent7out);
+		
+		corridor14.addExit(corridor14west);
+		corridor14.addExit(corridor14east);
+		corridor14.addExit(corridor14in);
+		
+		hangar.addExit(hangarin);
+		hangar.addExit(hangareast);
+		
+		
+		corridor15.addExit(corridor15in);
+		corridor15.addExit(corridor15west);
+		corridor15.addExit(corridor15north);
+		
+		corridor16.addExit(corridor16in);
+		corridor16.addExit(corridor16north);
+		corridor16.addExit(corridor16south);
+		
+		corridor17.addExit(corridor17in);
+		corridor17.addExit(corridor17north);
+		corridor17.addExit(corridor17south);
+		
+		vent8.addExit(vent8down);
+		vent8.addExit(vent8south);
+		vent8.addExit(vent8out);
 
 		// Add locations/exits to our game lists
 		game.addRoom(bunk);
@@ -231,6 +350,27 @@ public final class WorldCreation
 		game.addRoom(kitchenstorage);
 		game.addRoom(kitchenfreezer);
 		game.addRoom(vent4);
+		
+		game.addRoom(controlroom);
+		game.addRoom(corridor11);
+		game.addRoom(elevator4);		
+		game.addRoom(corridor12);
+		game.addRoom(cabin2);
+		game.addRoom(corridor13);
+		game.addRoom(vent7); 
+		game.addRoom(corridor14);
+		
+		game.addRoom(hangar);
+		game.addRoom(escapeshuttle);
+		
+		game.addRoom(corridor15);
+		game.addRoom(vent5);
+		game.addRoom(elevator3);
+		game.addRoom(corridor16);
+		game.addRoom(vent6);
+		
+		game.addRoom(corridor17);
+		game.addRoom(vent8);
 				
 		
 		game.addExit(bunkout);
@@ -253,9 +393,9 @@ public final class WorldCreation
 		game.addExit(corridor6south);
 	    game.addExit(vent1east);				
 	    game.addExit(vent1north);				
-		//game.addExit(vent1up);
+		game.addExit(vent1up);
 	    game.addExit(vent1out);
-		//game.addExit(elevator1up);
+		game.addExit(elevator1up);
 	    game.addExit(elevator1out);
 			               		                
 	    game.addExit(storageroomnorth);		
@@ -264,7 +404,7 @@ public final class WorldCreation
 			                       
 	    game.addExit(vent2west);				
 	    game.addExit(vent2north);				
-		//game.addExit(vent2up);
+		game.addExit(vent2up);
 	    game.addExit(vent2out);
 	    game.addExit(corridor7west);
 	    game.addExit(corridor7north);	 
@@ -275,12 +415,12 @@ public final class WorldCreation
 	    game.addExit(corridor2north);	 
 	    game.addExit(corridor2east);
 	    game.addExit(vent3south);
-	    //game.addExit(vent3up);
+	    game.addExit(vent3up);
 	    game.addExit(vent3out);
 	    game.addExit(corridor10in);	 
 	    game.addExit(corridor10east);	 
 	    game.addExit(corridor10north);	 
-	    //game.addExit(elevator2up);
+	    game.addExit(elevator2up);
 	    game.addExit(elevator2out);
 	    game.addExit(dininghalleast);	 
 	    game.addExit(dininghallwest);	 
@@ -297,10 +437,58 @@ public final class WorldCreation
 	    game.addExit(kitcheneast);	
 	    game.addExit(kitchensoutheast);	
 	    game.addExit(vent4south);
-	    //game.addExit(vent4up);
+	    game.addExit(vent4up);
 	    game.addExit(vent4out);
 	    game.addExit(kitchenstoragewest);
-		game.addExit(kitchenfreezerwest);				                   
+		game.addExit(kitchenfreezerwest);	
+		
+		game.addExit(controlroomsouthwest);
+		game.addExit(controlroomsoutheast);
+		
+		game.addExit(corridor11in);
+		game.addExit(corridor11north);
+		game.addExit(corridor11south);
+		
+		game.addExit(elevator4down);
+		game.addExit(elevator4out);
+
+		game.addExit(corridor12in);
+		game.addExit(corridor12north);
+		game.addExit(corridor12south);
+		
+		game.addExit(cabin2out);
+		
+		game.addExit(corridor13in);
+		game.addExit(corridor13south);
+		game.addExit(corridor13north);
+		
+		game.addExit(vent7down);
+		game.addExit(vent7south);
+		game.addExit(vent7out);
+		
+		game.addExit(corridor14west);
+		game.addExit(corridor14east);
+		game.addExit(corridor14in);
+		
+		game.addExit(hangarin);
+		game.addExit(hangareast);
+		
+		
+		game.addExit(corridor15in);
+		game.addExit(corridor15west);
+		game.addExit(corridor15north);
+		
+		game.addExit(corridor16in);
+		game.addExit(corridor16north);
+		game.addExit(corridor16south);
+		
+		game.addExit(corridor17in);
+		game.addExit(corridor17north);
+		game.addExit(corridor17south);
+		
+		game.addExit(vent8down);
+		game.addExit(vent8south);
+		game.addExit(vent8out);
 				
 		
 		// Set current location
